8c7ac89f3366c5849c3c9a9df1fe5737cd51b07b
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-05-09 00:15:37 +0300

    Updated the Page class

--- Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/Page.java
+++ Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/Page.java
@@ -8,20 +8,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public abstract class Page {
 
-    private final WebDriver driver;
+    private static final WebDriver driver = new ChromeDriver();
     private String title;
+    private static final String pageHeaderXpath = "/html/body/app-root/app-pet-add/div/div/h2";
+
 
     protected Page(String title) {
-        this.driver = new ChromeDriver();
         this.title = title;
     }
 
 
     public boolean isCurrent() {
-        return title.equals(driver.getTitle());
-    }
+        return title.equals(driver.findElement(By.xpath(pageHeaderXpath)).getText()); }
 
     protected void goTo(String url) {
         driver.get(url);
@@ -35,14 +38,27 @@ public abstract class Page {
         return driver.findElement(By.xpath(cssPath)).getText();
     }
 
+    protected List<WebElement> getElements(String xPath){return driver.findElements(By.xpath(xPath));}
+
     protected void fill(String id, String value) {
         final WebElement element = waitFor(id);
         element.clear();
         element.sendKeys(value);
     }
 
+    protected void cssFill(String cssPath, String value) {
+        final WebElement element = cssWaitFor(cssPath);
+        element.clear();
+        element.sendKeys(value);
+    }
+
     protected void selectFirst(String id) {
         new Select(driver.findElement(By.id(id))).selectByIndex(1);
+    }
+
+
+    protected void select(String id , int i) {
+        new Select(driver.findElement(By.id(id))).selectByIndex(i-1);
     }
 
     protected void click(String id) {
@@ -55,6 +71,18 @@ public abstract class Page {
 
     private WebElement waitFor(String id, int waitInterval) {
         return (new WebDriverWait(driver, waitInterval)).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+    }
+
+    private WebElement cssWaitFor(String cssPath) {
+        return cssWaitFor(cssPath, 5);
+    }
+
+    private WebElement cssWaitFor(String cssPath, int waitInterval) {
+        return (new WebDriverWait(driver, waitInterval)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssPath)));
+    }
+
+    protected void implicitlyWait(int sec){
+        driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS) ;
     }
 
     protected boolean exists(String id) {
