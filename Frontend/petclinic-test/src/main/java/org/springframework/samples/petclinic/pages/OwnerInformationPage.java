a85c47e851eeee2ec0f9446c8e39ba3ec4f1eab9
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-05-09 03:28:16 +0300

    Updated the lastAddedPetName function to return list of strings

--- Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/OwnerInformationPage.java
+++ Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/OwnerInformationPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class OwnerInformationPage extends Page {
 
@@ -13,10 +14,13 @@ public class OwnerInformationPage extends Page {
     }
 
 
-    public String lastAddedPetName(){
+    public List<String> lastAddedPetName(){
         implicitlyWait(1);
         List<WebElement> pets = getElements("/html/body/app-root/app-owner-detail/div/div/table[2]/tr/app-pet-list/table/tr/td[1]/dl/dd[1]");
-        return pets.get(pets.size()-1).getText();
+        //return pets.get(pets.size()-1).getText();
+        return pets.stream()
+                .map(pet -> pet.getText())
+                .collect(Collectors.toList());
     }
 
 }
