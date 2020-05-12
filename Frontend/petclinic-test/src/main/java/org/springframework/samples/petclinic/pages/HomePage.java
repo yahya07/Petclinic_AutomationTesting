62eae11d98726c4adb9356ca34c6e6bf69740004
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-05-12 05:08:15 +0300

    refactored homepage variable from static to local

--- Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/HomePage.java
+++ Frontend/petclinic-test/src/main/java/org/springframework/samples/petclinic/pages/HomePage.java
@@ -1,26 +1,21 @@
 package org.springframework.samples.petclinic.pages;
 
-import io.cucumber.java.After;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.samples.petclinic.pages.vet.AddVetPage;
 import org.springframework.samples.petclinic.pages.vet.AllVetsPage;
-
-import java.util.concurrent.TimeUnit;
 
 public class HomePage extends Page {
 
     private static final String URL = "http://localhost:8081/petclinic/";
 
-    private static final String PATH_OWNERS_DROPDOWN = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(2) > a";
-    private static final String PATH_OWNERS = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(1) > a";
-    private static final String PATH_ADD_OWNER = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(2) > a";
-    private static final String PATH_VETS_DROPDOWN = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(3) > a";
-    private static final String PATH_VETS = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(1) > a";
-    private static final String PATH_ADD_VET = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(2) > a";
-    private static final String PATH_PET_TYPES = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(4) > a";
-    private static final String PATH_SPECIALTIES = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(5) > a";
+    private String pathOwnersDropdown = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(2) > a";
+    private String pathOwners = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(1) > a";
+    private String pathAddOwner = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(2) > a";
+    private String pathVetsDropdown = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(3) > a";
+    private String pathVets = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(1) > a";
+    private String pathAddVet = "body > app-root > div.container-fluid > nav > div > ul > li.dropdown.open > ul > li:nth-child(2) > a";
+    private String pathPetTypes = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(4) > a";
+    private String pathSpecialties = "body > app-root > div.container-fluid > nav > div > ul > li:nth-child(5) > a";
 
 
 
@@ -35,8 +30,8 @@ public class HomePage extends Page {
     }
 
     public AddOwnerPage goToAddOwner() {
-        cssClick(PATH_OWNERS_DROPDOWN);
-        cssClick(PATH_ADD_OWNER);
+        cssClick(pathOwnersDropdown);
+        cssClick(pathAddOwner);
         return new AddOwnerPage();
     }
 
@@ -44,21 +39,21 @@ public class HomePage extends Page {
 
 
     public AllOwnersPage goToOwnersList(){
-        cssClick(PATH_OWNERS_DROPDOWN);
-        cssClick(PATH_OWNERS);
+        cssClick(pathOwnersDropdown);
+        cssClick(pathOwners);
         return new AllOwnersPage(driver);
     }
 
 
     public AddVetPage goToAddVet(){
-        cssClick(PATH_VETS_DROPDOWN);
-        cssClick(PATH_ADD_VET);
+        cssClick(pathVetsDropdown);
+        cssClick(pathAddVet);
         return new AddVetPage(driver);
     }
 
     public AllVetsPage goToVetsList(){
-        cssClick(PATH_VETS_DROPDOWN);
-        cssClick(PATH_VETS);
+        cssClick(pathVetsDropdown);
+        cssClick(pathVets);
         return new AllVetsPage(driver);
     }
 
